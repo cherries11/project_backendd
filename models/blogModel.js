e4c1035b786ae53f700cebb4d7133793a9046528
cherries <150523522+cherries11@users.noreply.github.com>
2024-05-10 21:25:48 +0100

    Add files via upload

---
+++ models/blogModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const blogPostSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User", // Reference to the user who authored the post
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("BlogPost", blogPostSchema);
